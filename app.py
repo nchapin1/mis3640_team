7f0cf2224f47872765f35895feac73cf081f7204
nchapin1 <70173250+nchapin1@users.noreply.github.com>
2020-11-30 17:11:50 -0500

    update

--- app.py
+++ app.py
@@ -1,96 +1,157 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
-from database import db, User, app
+from sqlalchemy.ext.indexable import index_property
+from flask_mail import Mail
+import json
+from requests import Session
+import os
+from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.engine import create_engine
+
+
+# engine = create_engine("sqlite:///users.db", echo=True)
+# meta = MetaData(engine)
+
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SECRET_KEY"] = "asldkf0cm0iasfew"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+db = SQLAlchemy(app)
+
+
+app.config["MAIL_SERVER"] = "smtp.mail.yahoo.com"
+app.config["MAIL_PORT"] = 587
+# app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = True #False
+app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS")
+mail = Mail(app)
+
+
+def crypto_current(symbol):
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+    parameters = {
+        "start": "1",
+        "limit": "5000",
+        "convert": "USD",
+    }
+    headers = {
+        "Accepts": "application/json",
+        "X-CMC_PRO_API_KEY": "ce4157ad-edeb-4e93-83f0-bb977f31d3e3",
+    }
+
+    session = Session()
+    session.headers.update(headers)
+    response = session.get(url, params=parameters)
+    raw = json.loads(response.text)
+    data = raw['data']
+
+    for item in data:
+        if item["symbol"] == symbol:
+            current = item["quote"]["USD"]["price"]
+            break
+    return current
+
+
+# USER CLASS SETUP
+class User(db.Model):
+    def __init__(self, username, email, phone, password):
+        self.username = username
+        self.email = email
+        self.phone = phone
+        self.password = password
+
+    def __str__(self):
+        return self.username
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30))
+    email = db.Column(db.String(50))
+    password = db.Column(db.String(30))
+    crypto = db.relationship(
+        "Crypto", backref="user", lazy=True)
+    
+# CRYPTO CLASS SETUP
+class Crypto(db.Model):
+    def __init__(self, symbol, purchase, current, target, tolerance, username_id):
+        self.symbol = symbol
+        self.purchase = purchase
+        self.current = current
+        self.target = target
+        self.tolerance = tolerance
+        self.username_id = username_id
+
+    symbol = db.Column(db.String(30), primary_key=True)
+    purchase = db.Column(db.Numeric(10))
+    current = db.Column(db.Numeric(10))
+    target = db.Column(db.Numeric(10))
+    tolerance = db.Column(db.Numeric(10))
+    username_id = db.Column(db.String, db.ForeignKey("user.username"), nullable=False)
+
+db.create_all()
 
 # ERROR PAGE
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("page_not_found.html"), 404
 
-
 # INDEX PAGE
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# HOME
-@app.route("/home/")
-def home():
-    return render_template("home.html")
+# @app.route('/login/')
+# def login():
+#     return render_template('login.html')
 
 # LOGIN PAGE
-@app.route("/login/", methods=['GET', "POST"])
-def login_user():
-    # if request.method=="POST":
-    input_username = str(request.form['uname']),
-    input_password = str(request.form['password'])
-    if input_username and input_password:
-        existing_user = User.query.filter(
-            User.username == input_username and User.email == input_password
-        ).first()
-        if existing_user:
-            return render_template("home.html")
+@app.route("/login_user_request", methods=["GET", "POST"])
+def login_request():
+    if request.method == "POST":
+        input_username = str(request.form["uname"])
+        input_password = str(request.form["password"])
+        username = str(User.query.get(input_username))
+        password = str(User.query.filter_by(password=input_password).first())
+        if input_username == username and input_password == password:
+            # return render_template('home.html', username = username)
+            return redirect(url_for('homepage', username=username))
         else:
-            flash('not working', "error")
+            flash('Incorrect Username or Password', 'error')
+    return render_template('login.html')
+# https://github.com/mattupstate/flask-security/issues/198
 
-
-        query_username = User.query.get(input_username)
-        # query_username = Users.query.filter_by(username=input_username)
-        # query_password = Users.query.filter_by(password=input_password)
-
-        # if input_username == query_username and input_password == query_password:
-        #     return redirect(url_for('home'))
-        return render_template("home.html", input_username=input_username, input_password=input_password, query_username=query_username)
-    return render_template("login.html")
-
+# @app.route('/registration/')
+# def register():
+#     return render_template("register.html")
 
 # SIGN UP CREATE USER
-@app.route("/signup/", methods=["GET", "POST"])
-def create_user():
-    
+@app.route("/registration_user_request", methods=["GET", "POST"])
+def register_request():
     if request.method == "POST":
-        user = User(
-        username=request.form["uname"],
-        email=request.form["email"],
-        phone=request.form["pnumber"],
-        password=request.form["password"])
-    
-        db.session.add(user)
+        username = str(request.form["uname"])
+        email = str(request.form["email"])
+        phone = str(request.form["pnumber"])
+        password = str(request.form["password"])
+
+        new_user = User(username, email, phone, password)
+        db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('home'))
-    else:
-        return render_template('signup.html')
-    # return render_template('signup.html')
+        return redirect(url_for('homepage', username=username))  
+    return render_template('register.html') 
 
-    # WORKS return render_template('home.html', user=user)
+# HOME
+@app.route("/home_page/<username>")
+def homepage(username):
+    rows = Crypto.query.all()
+    return render_template("home.html", title="Investments", rows=rows)
+    
 
-# if __name__ == '__main__':
-#     db.create_all()
-#     app.run(debug=True)
-
-
-
-
-# @app.route("/", methods=["GET"])
-# def user_records():
-#     """Create a user via query string parameters."""
-#     username = request.args.get("user")
-#     email = request.args.get("email")
-#     if username and email:
-#         existing_user = User.query.filter(
-#             User.username == username or User.email == email
-#         ).first()
-#         if existing_user:
-#             return make_response(f"{username} ({email}) already created!")
-#         new_user = User(
-#             username=username,
-#             email=email,
-#             created=dt.now(),
-#             bio="In West Philadelphia born and raised, \
-#             on the playground is where I spent most of my days",
-#             admin=False,
-#         )  # Create an instance of the User class
-#         db.session.add(new_user)  # Adds new User record to database
-#         db.session.commit()  # Commits all changes
-#         redirect(url_for("user_records"))
-#     return render_template("users.jinja2", users=User.query.all(), title="Show Users")
+       
--- database.py
+++ database.py
@@ -1,69 +1,94 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData, Table, Column, Integer, String
+from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.engine import create_engine
-from sqlalchemy.ext.compiler import compiles
-from sqlalchemy.sql.expression import Executable, ClauseElement
-engine = create_engine('sqlite:///user.db', echo = True)
-meta=MetaData(engine)
+
+# from sqlalchemy.ext.compiler import compiles
+# from sqlalchemy.sql.expression import Executable, ClauseElement
+engine = create_engine("sqlite:///users.db", echo=True)
+meta = MetaData(engine)
 
 
-# APP SETUP
+# SQLALCHEMY
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db" 
-app.config['SECRET_KEY'] = 'asldkf0cm0iasfew'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SECRET_KEY"] = "asldkf0cm0iasfew"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
-# class CreateView(Executable, ClauseElement):
-#     def __init__(self, username, email, phone, password):
-#         self.username = username
-#         self.email = email
-#         self.phone = phone
-#         self.password=password
 
-# @compiles(CreateView)
-# def visit_create_view(element, compiler, **kw):
-#     return "CREATE VIEW %s AS %s" % (
-#          element.name,
-#          compiler.process(element.select, literal_binds=True)
-#          )
 
-# DATABASE TABLE SETUP
-user = Table(
-    'users', meta,
-    Column('id', Integer, primary_key = True), 
-    Column('username', String), 
-    Column('email', String), 
-    Column('phone', Integer),
-    Column('password', String),
+db = SQLAlchemy(app)
+
+from flask_security import (
+    Security,
+    SQLAlchemyUserDatastore,
+    UserMixin,
+    RoleMixin,
+    login_required,
 )
-user.create()
 
-# # create view
-# createview = CreateView('viewname', users.select().where(users.c.id>5))
-# engine.execute(createview)
+roles_users = db.Table(
+    "roles_users",
+    db.Column("user_id", db.Integer(), db.ForeignKey("user.username")),
+    db.Column("role_id", db.Integer(), db.ForeignKey("role.id")),
+)
 
-# # reflect view and print result
-# v = Table('viewname', meta, autoload=True)
-# for r in engine.execute(v.select()):
-#     print (r)
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
 
-# meta.create_all(engine)
+from flask_mail import Mail
 
-# DATABASE CLASS SETUP
-class User(db.Model):
-    id = db.Column('user_id', db.Integer, primary_key=True)
-    username = db.Column(db.String(30))
-    email = db.Column(db.String(50))
-    phone = db.Column(db.Integer)
-    password = db.Column(db.String(30))
+app.config['MAIL_SERVER'] = 'smtp.example.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'username'
+app.config['MAIL_PASSWORD'] = 'password'
+mail = Mail(app)
 
+# USER CLASS SETUP
+class User(db.Model, UserMixin):
     def __init__(self, username, email, phone, password):
         self.username = username
         self.email = email
         self.phone = phone
         self.password = password
-    
+
     def __str__(self):
         return self.username
+
+    __tablename__ = "user"
+    username = db.Column(db.String(30), primary_key=True)
+    email = db.Column(db.String(50))
+    phone = db.Column(db.Integer)
+    password = db.Column(db.String(30))
+    cryptos = db.relationship(
+        "Crypto", lazy="select", backref=db.backref("user", lazy="joined")
+    )
+
+
+# CRYPTO CLASS SETUP
+class Crypto(db.Model, UserMixin):
+    def __init__(self, symbol, purchase, current, target, tolerance, username_id):
+        self.symbol = symbol
+        self.purchase = purchase
+        self.current = current
+        self.target = target
+        self.tolerance = tolerance
+        self.username_id = username_id
+
+    __tablename__ = "crypto"
+    symbol = db.Column(db.String(30), primary_key=True)
+    purchase = db.Column(db.Numeric(10))
+    current = db.Column(db.Numeric(10))
+    target = db.Column(db.Numeric(10))
+    tolerance = db.Column(db.Numeric(10))
+    username_id = db.Column(db.String, db.ForeignKey("user.username"), nullable=False)
+
+    def __str__(self):
+        return self.username
+
+
+db.create_all()
